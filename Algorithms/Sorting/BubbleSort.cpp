f63670233a50411636152866e3098bd7319e7571
youssefkhaled08 <ykfhe08@gmail.com>
2021-01-18 18:19:56 +0200

    Added Bubble Sort

---
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int*, int*);
+void bubbleSort(int*, int);
+
+int main (void){
+    int size;
+    cout << "\nEnter the size of the Array : ";
+    cin >> size;
+
+    int *array = new int[size];
+    
+
+
+    cout << "\nEnter the Array of " << size << " numbers to be sorted: ";
+    for (int i = 0; i < size; i++) {
+        cin >> array[i];
+    }
+
+    bubbleSort(array, size);
+    
+    cout << "\nThe Sorted Array: ";
+    for (int i = 0; i < size; i++) {
+        cout << array[i] << " ";
+    }
+
+
+
+    delete[] array;
+    return 0;
+}
+void swap(int* x, int* y){
+    int z = *x;
+    *x = *y;
+    *y = z;
+}
+void bubbleSort(int* arr, int size){
+    bool isSorted = false;
+    while (!isSorted){
+        isSorted = true;
+        for(int i = 0; i < size - 1; i++){
+            if(arr[i] > arr[i + 1]){
+                swap(arr[i], arr[i + 1]);
+                isSorted  = false;
+            }
+        }
+            size--;
+    }
+}
