0280e0df338d51082064879052db55e17e319c67
youssefkhaled08 <ykfhe08@gmail.com>
2021-01-19 15:33:32 +0200

    Organized and Commented

--- Algorithms/Sorting/BubbleSort.cpp
+++ Algorithms/Sorting/BubbleSort.cpp
@@ -53,11 +53,13 @@ void bubbleSort(int* arr, int size){
     }
 }
 void recursiveBubbleSort(int* arr, int size){
-    if (size == 1)
+    if (size == 1){
         return;
+    }
     for (int i = 0; i < size - 1; i++){
-        if(arr[i] > arr[i + 1])
+        if(arr[i] > arr[i + 1]){
             swap(arr[i], arr[i + 1]);
+        }
     }
     recursiveBubbleSort(arr, size - 1);
 }
