7cab9a38b62121e9ced7b10193f113107505767a
Youssef Khaled <youssefkfarouk@gmail.com>
2021-04-28 01:25:25 +0200

    Commented and organized.

--- DS - Queue/Queue.h
+++ DS - Queue/Queue.h
@@ -11,11 +11,11 @@ private:
     int back;
 public:
     Queue(int val);
-    void Enqueue(dataType val);
-    void Dequeue();
-    dataType Front();
-    int length();
-    bool isEmpty();
-    bool isFull();
+    void Enqueue(dataType val);                 // Adds an element to the end of the queue.
+    void Dequeue();                             // Removes the element at the front of the queue.
+    dataType Front();                           // Returns the element at the front of the queue.
+    int length();                               // Returns the number of elements in the queue.
+    bool isEmpty();                             // Returns true if the queue is empty.
+    bool isFull();                              // Returns true if the queue is full.
     ~Queue();
 };
--- DS - Stack/stack.h
+++ DS - Stack/stack.h
@@ -9,11 +9,11 @@ class Stack
 
     public:
     Stack(int val);
-    void push(dataType num);
-    void pop();
-    int length();
-    dataType top();
-    bool isEmpty();
-    void expand();
+    void push(dataType num);                    // Adds an element to the top of the stack.
+    void pop();                                 // Removes an elements from the top of the stack.
+    int length();                               // Returns the number of elements in the stack.
+    dataType top();                             // Returns the element at the top of the stack.
+    bool isEmpty();                             // Returns true if the stack is empty.
+    void expand();                              // Doubles the size of the stack.
     ~Stack();
 };
