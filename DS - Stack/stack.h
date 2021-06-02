9062a3c77349555ff54ba028d716943f08620073
Youssef Khaled <youssefkfarouk@gmail.com>
2021-06-02 16:12:00 +0200

    Organized and Commented.

--- DS - Stack/stack.h
+++ DS - Stack/stack.h
@@ -9,12 +9,12 @@ class Stack
     int size;
 
     public:
-    Stack(int val);
-    void push(dataType num);                    // Adds an element to the top of the stack.
-    void pop();                                 // Removes an elements from the top of the stack.
-    int length();                               // Returns the number of elements in the stack.
-    dataType top();                             // Returns the element at the top of the stack.
-    bool isEmpty();                             // Returns true if the stack is empty.
-    void expand();                              // Doubles the size of the stack.
-    ~Stack();
+    Stack(int);
+    void push(dataType);                    // Adds an element to the top of the stack.
+    void pop();                             // Removes an elements from the top of the stack.
+    int length();                           // Returns the number of elements in the stack.
+    dataType top();                         // Returns the element at the top of the stack.
+    bool isEmpty();                         // Returns true if the stack is empty.
+    void expand();                          // Doubles the size of the stack.
+    ~Stack();                               // Frees memory allocated for the stack.
 };
